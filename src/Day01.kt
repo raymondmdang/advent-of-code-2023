8eaf3068a28cb2d830013a945c4421b4941c89d5
raymondmdang <60908057+raymondmdang@users.noreply.github.com>
2023-12-01 20:51:35 -0800

    feat: add day one brute force solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.filter(Char::isDigit) }
+            .map { (it.first().toString() + it.last().toString()).toInt() }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val letters = listOf(
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+        )
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val numbers = listOf(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
+        )
+
+        val map = letters.zip(numbers).toMap() + numbers.zip(letters).toMap()
+        val digits = List(input.size) { 0 }.toMutableList();
+        for (idx in input.indices) {
+            var first = input[idx].findAnyOf(map.keys)?.second
+            var second = input[idx].findLastAnyOf(map.keys)?.second
+            if (letters.contains(first)) {
+                first = map[first]
+            }
+            if (letters.contains(second)) {
+                second = map[second]
+            }
+            digits[idx] = (first.toString() + second.toString()).toInt()
+        }
+
+        return digits.sum()
+    }
 
     val input = readInput("Day01")
     part1(input).println()
